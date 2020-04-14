e2e25e3efe4be6b069095982fb06f3ba84b95466
karifl <lampi_88@hotmail.com>
2020-04-14 15:59:47 +0300

    Update Arvuuttaja.java

--- Arvuuttaja.java
+++ Arvuuttaja.java
@@ -30,6 +30,31 @@ public class Arvuuttaja {
 		
 
 	}
+	private class Memento {
+
+	private int state;
+	private StringBuilder content; //säilöö stringin
+	
+		
+	public Memento(int state, StringBuilder content) {
+		this.state = state;
+		this.content=new StringBuilder(content);
+	}
+	
+	
+	public int getState() {
+		return this.state;
+	}
+	
+	public void setState(int state) {
+		this.state = state;
+	}
+	
+	public StringBuilder getContent() {
+		return content;
+	}
+	
+}
 	
 	
 
